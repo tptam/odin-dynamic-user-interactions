f07debb2aa9d52eef8f6438523e6941cd2c5faec
tptam <99264397+tptam@users.noreply.github.com>
2024-07-24 14:21:45 +0900

    Modify to call setupDropdownMenu

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-const content = document.querySelector("#content");
-const title = document.createElement("h1");
-const para = document.createElement("p");
-title.textContent = "Hello world!";
-para.textContent = "This is a sample page created as a template.";
+import { setupDropdownMenu } from "./dropdown-button";
+import "./style.css";
 
-content.appendChild(title);
-content.appendChild(para);
+setupDropdownMenu();
