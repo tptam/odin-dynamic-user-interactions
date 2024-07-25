5cde40d796cd638609cb307af0fc7c2a7865cfcb
tptam <99264397+tptam@users.noreply.github.com>
2024-07-25 15:49:04 +0900

    Modify sample

--- src/index.js
+++ src/index.js
@@ -13,10 +13,10 @@ nextSvg.src = next;
 const prevSvg = new Image(40, 80);
 prevSvg.src = prev;
 
-const carousel = createCarousel(
-  "300px",
-  "200px",
-  "400px",
+const carousel1 = createCarousel(
+  300,
+  200,
+  400,
   [
     {
       src: "https://picsum.photos/200/200?random=1",
@@ -41,7 +41,63 @@ const carousel = createCarousel(
   ],
   false,
   nextSvg,
-  prevSvg
+  prevSvg,
+  undefined,
+  undefined,
+  undefined,
+  "#f5f5f5"
 );
 
-document.querySelector("#content").appendChild(carousel);
+const carousel2 = createCarousel(
+  300,
+  300,
+  300,
+  [
+    {
+      src: "https://picsum.photos/500/500?random=6",
+      alt: "random dummy image",
+    },
+    {
+      src: "https://picsum.photos/500/500?random=7",
+      alt: "random dummy image",
+    },
+    {
+      src: "https://picsum.photos/500/500?random=8",
+      alt: "random dummy image",
+    },
+    {
+      src: "https://picsum.photos/500/500?random=9",
+      alt: "random dummy image",
+    },
+    {
+      src: "https://picsum.photos/500/500?random=10",
+      alt: "random dummy image",
+    },
+  ],
+  true,
+  null,
+  null,
+  24,
+  "#fecdd3",
+  "#f43f5e"
+);
+
+const title = document.createElement("h1");
+title.textContent = "Carousels";
+const sub1 = document.createElement("h2");
+sub1.textContent =
+  "Wider window than slide, custom arrow icons, custom background color, default dots";
+const sub2 = document.createElement("h2");
+sub2.textContent =
+  "Same window and slide widths, default arrow icons, custom icon color and dot size, auto-rotate";
+
+sub2.style.marginTop = "4rem";
+carousel1.style.margin = "1rem auto";
+carousel2.style.margin = "1rem auto";
+
+const content = document.querySelector("#content");
+content.appendChild(title);
+content.appendChild(sub1);
+content.appendChild(carousel1);
+content.appendChild(sub2);
+content.appendChild(carousel2);
